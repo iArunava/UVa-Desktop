8f8efa35cabbb2b08d7a888bb3b636c449906a0d
Arunava <learningdroidarunava@gmail.com>
2017-08-26 20:47:23 +0530

    Added remaining Volumes (AOAPCi 1, 1, 2  related and CP 1, 2, 3)

--- app/js/volumeCtrlr.js
+++ app/js/volumeCtrlr.js
@@ -16,12 +16,7 @@ app.config (function ($routeProvider) {
     .when ("/problemSetVol", {
         template: '<li ng-repeat="folder in range (1, 18)">' +
                          '<h4><a href="{{ folder.link }}">' + 
-                             'Volume {{ folder }}' +
-                             '({{ folder + "00" }} - {{ folder + "99" }})' +
-                         '</a></h4>' +
-                         '<hr />' +
-                   '</li>'
-    })
+                             'Volume {{ folder }}' + '({{ folder + "00" }} - {{ folder + "99" }})' + '</a></h4>' + '<hr />' + '</li>' })
 
     .when ("/contestVol", {
         template: '<li ng-repeat="folder in range (100, 133)">' +
@@ -77,6 +72,61 @@ app.config (function ($routeProvider) {
                          '<hr />' +
                    '</li>'
     })
+
+    .when ("/aoapc1", {
+        template: '<li ng-repeat="folder in range (0, 8)">' +
+                         '<h4><a href="{{ folder.link }}">' + 
+                             'Volume {{ folder }}. {{aoapc1dir[folder]}}' +
+                         '</a></h4>' +
+                         '<hr />' +
+                   '</li>'
+    })
+
+    .when ("/aoapc1bgc", {
+        template: '<li ng-repeat="folder in range (1, 7)">' +
+                         '<h4><a href="{{ folder.link }}">' + 
+                             'Chapter {{ folder }}. {{aoapc1dir2[folder]}}' +
+                         '</a></h4>' +
+                         '<hr />' +
+                   '</li>'
+    })
+
+    .when ("/aoapc2", {
+        template: '<li ng-repeat="folder in range (3, 13)">' +
+                         '<h4><a href="{{ folder.link }}">' + 
+                             'Chapter {{ folder }}. {{aoapc2dir[folder]}}' +
+                         '</a></h4>' +
+                         '<hr />' +
+                   '</li>'
+    })
+
+    .when ("/cp1", {
+        template: '<li ng-repeat="folder in range (1, 8)">' +
+                         '<h4><a href="{{ folder.link }}">' + 
+                             'Chapter {{ folder }}. {{cp1dir[folder]}}' +
+                         '</a></h4>' +
+                         '<hr />' +
+                   '</li>'
+    })
+
+    .when ("/cp2", {
+        template: '<li ng-repeat="folder in range (0, 8)">' +
+                         '<h4><a href="{{ folder.link }}">' + 
+                             '{{cp2dir[folder]}}' +
+                         '</a></h4>' +
+                         '<hr />' +
+                   '</li>'
+    })
+
+    .when ("/cp3", {
+        template: '<li ng-repeat="folder in range (0, 9)">' +
+                         '<h4><a href="{{ folder.link }}">' + 
+                             '{{ cp3dir[folder] }}' +
+                         '</a></h4>' +
+                         '<hr />' +
+                   '</li>'
+    })
+
 });
 
 app.controller ("volumeCtrlr", function ($scope) {
@@ -170,6 +220,66 @@ app.controller ("volumeCtrlr", function ($scope) {
     $scope.rujiaLiuDir [5] = "Developing Simplified Softwares";
     $scope.rujiaLiuDir [6] = "Happy 30th Birthday to Myself";
     $scope.rujiaLiuDir [7] = "Hello, interactive problems!";
+    
+    $scope.aoapc1dir = {};
+    $scope.aoapc1dir [0] = "Getting Started";
+    $scope.aoapc1dir [1] = "Elementary Problem Solving";
+    $scope.aoapc1dir [2] = "Data Structures";
+    $scope.aoapc1dir [3] = "Brute Force";
+    $scope.aoapc1dir [4] = "Algorithm Design";
+    $scope.aoapc1dir [5] = "Dynamic Programming";
+    $scope.aoapc1dir [6] = "Mathematical Concepts and Methods";
+    $scope.aoapc1dir [7] = "Graph Algorithms and Implementation Techniques";
+    
+    $scope.aoapc1dir2 = {};
+    $scope.aoapc1dir2 [1] = "Algorithm Design";
+    $scope.aoapc1dir2 [2] = "Mathematics";
+    $scope.aoapc1dir2 [3] = "Data Structures";
+    $scope.aoapc1dir2 [4] = "Geometry";
+    $scope.aoapc1dir2 [5] = "Graph Theory";
+    $scope.aoapc1dir2 [6] = "Selected Topics";
+    
+    $scope.aoapc2dir = {};
+    $scope.aoapc2dir [3]  = "Arrays and Strings";
+    $scope.aoapc2dir [4]  = "Functions and Recursion";
+    $scope.aoapc2dir [5]  = "C++ and STL";
+    $scope.aoapc2dir [6]  = "Data Structures";
+    $scope.aoapc2dir [7]  = "Brute Force";
+    $scope.aoapc2dir [8]  = "Algorithm Design";
+    $scope.aoapc2dir [9]  = "Dynamic Programming";
+    $scope.aoapc2dir [10] = "Maths";
+    $scope.aoapc2dir [11] = "Graph Theory";
+    $scope.aoapc2dir [12] = "Advanced Topics";
+    
+    $scope.cp1dir = {};
+    $scope.cp1dir [1] = "Introduction";
+    $scope.cp1dir [2] = "Data Structures and Libraries";
+    $scope.cp1dir [3] = "Problem Solving Paradigms";
+    $scope.cp1dir [4] = "Graph";
+    $scope.cp1dir [5] = "Mathematics";
+    $scope.cp1dir [6] = "String Processing";
+    $scope.cp1dir [7] = "(Computational) Geometry";
+    
+    $scope.cp2dir = {};
+    $scope.cp2dir [0] = "Introduction";
+    $scope.cp2dir [1] = "Data Structures and Libraries";
+    $scope.cp2dir [2] = "Problem Solving Paradigms";
+    $scope.cp2dir [3] = "Graph";
+    $scope.cp2dir [4] = "Mathematics";
+    $scope.cp2dir [5] = "String Processing";
+    $scope.cp2dir [6] = "(Computational) Geometry";
+    $scope.cp2dir [7] = "More Advanced Topics";
+
+    $scope.cp3dir = {};
+    $scope.cp3dir [0] = "Introduction";
+    $scope.cp3dir [1] = "Data Structures and Libraries";
+    $scope.cp3dir [2] = "Problem Solving Paradigms";
+    $scope.cp3dir [3] = "Graph";
+    $scope.cp3dir [4] = "Mathematics";
+    $scope.cp3dir [6] = "String Processing";
+    $scope.cp3dir [7] = "(Computational) Geometry";
+    $scope.cp3dir [8] = "More Advanced Topics";
+    $scope.cp3dir [9] = "Introduction";
 
     $scope.directories = [
     {   name: "Problem Set Volumes (100...1999)",
@@ -200,22 +310,22 @@ app.controller ("volumeCtrlr", function ($scope) {
         link: "#!rujiaLiu"
     },
     {   name: "AOAPC I: Beginning Algorithm Contests (Rujia Liu)",
-        link: "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1"
+        link: "#!aoapc1"
     },
     {   name: "AOAPC I: Beginning Algorithm Contests -- Training Guide (Rujia Liu)",
-        link: "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1"
+        link: "#!aoapc1bgc"
     },
     {   name: "AOAPC II: Beginning Algorithm Contests (Second Edition) (Rujia Liu)",
-        link: "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1"
+        link: "#!aoapc2"
     },
     {   name: "Competitive Programming: Increasing the Lower Bound of Programming Contests (Steven & Felix Halim)",
-        link: "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1"
+        link: "#!cp1"
     },
     {   name: "Competitive Programming 2: This increases the lower bound of Programming Contests. Again (Steven & Felix Halim)",
-        link: "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1"
+        link: "#!cp2"
     },
     {   name: "Competitive Programming 3: The New Lower Bound of Programming Contests (Steven & Felix Halim)",
-        link: "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1"
+        link: "#!cp3"
     },
 ];
 });
